5335f4a5cd780dcec03b9b045b96c917abd2db8d
Mouhyi-eddine <moutarajji.mouhyi@gmail.com>
2019-02-08 12:04:49 +0100

    added maven project ( Back)

---
+++ back_sit/src/main/java/fr/univ_rennes1/istic/sit/groupe2/back_sit/BackSitApplication.java
@@ -0,0 +1,14 @@
+package fr.univ_rennes1.istic.sit.groupe2.back_sit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackSitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackSitApplication.class, args);
+    }
+
+}
+
---
+++ back_sit/src/test/java/fr/univ_rennes1/istic/sit/groupe2/back_sit/BackSitApplicationTests.java
@@ -0,0 +1,17 @@
+package fr.univ_rennes1.istic.sit.groupe2.back_sit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BackSitApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
