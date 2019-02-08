77465bb3f9b69d816d11b23f45ef213d6f02c3b4
Gautier Rouleau <gautier.rouleau@gmail.com>
2019-02-08 11:22:40 +0100

    initial commit, added android studio project

---
+++ android/Dronemissionplanner/app/src/main/java/fr/univ_rennes1/istic/sit/groupe2/dronemissionplanner/MainActivity.java
@@ -0,0 +1,13 @@
+package fr.univ_rennes1.istic.sit.groupe2.dronemissionplanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
