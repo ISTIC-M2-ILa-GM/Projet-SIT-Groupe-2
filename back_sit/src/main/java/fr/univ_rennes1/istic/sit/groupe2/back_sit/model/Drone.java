4f9a59933f214d35b6b2a5cff7c78ed4e3de120c
Mouhyi-eddine <moutarajji.mouhyi@gmail.com>
2019-02-11 21:52:13 +0100

    mon super commit Mouhyi & Maxime

---
+++ back_sit/src/main/java/fr/univ_rennes1/istic/sit/groupe2/back_sit/model/Drone.java
@@ -0,0 +1,19 @@
+package fr.univ_rennes1.istic.sit.groupe2.back_sit.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Drone {
+
+    private long id;
+
+    public Drone() {
+
+    }
+
+    public Drone(long id) {
+        this.id = id;
+    }
+
+}
