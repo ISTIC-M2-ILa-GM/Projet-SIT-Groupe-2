2714fdec55c5e60ab8d9834af7da5c60784a1831
Mouhyi-eddine <moutarajji.mouhyi@gmail.com>
2019-02-11 21:52:13 +0100

    mon super commit Mouhyi & Maxime

---
+++ back_sit/src/main/java/fr/univ_rennes1/istic/sit/groupe2/back_sit/model/Position.java
@@ -0,0 +1,23 @@
+package fr.univ_rennes1.istic.sit.groupe2.back_sit.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter@Setter
+public class Position {
+
+    private long id;
+    private float lat;
+    private float lon;
+
+
+    public Position() {
+
+    }
+
+    public Position(long id, float lat, float lon) {
+        this.id = id;
+        this.lat = lat;
+        this.lon = lon;
+    }
+}
