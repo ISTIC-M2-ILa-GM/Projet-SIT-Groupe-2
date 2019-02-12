49937a5d7696c6bef62e423cb039a4b98b85048b
Mouhyi-eddine <moutarajji.mouhyi@gmail.com>
2019-02-12 16:24:38 +0100

    Modifcation du Dao

--- back_sit/src/main/java/fr/univ_rennes1/istic/sit/groupe2/back_sit/dao/MissionDao.java
+++ back_sit/src/main/java/fr/univ_rennes1/istic/sit/groupe2/back_sit/dao/MissionDao.java
@@ -1,7 +1,9 @@
 package fr.univ_rennes1.istic.sit.groupe2.back_sit.dao;
 
-import fr.univ_rennes1.istic.sit.groupe2.back_sit.model.Mission;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MissionDao extends JpaRepository<Mission,Long> {
+
+import fr.univ_rennes1.istic.sit.groupe2.back_sit.models.Mission;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface MissionDao extends MongoRepository<Mission,Long> {
 }
