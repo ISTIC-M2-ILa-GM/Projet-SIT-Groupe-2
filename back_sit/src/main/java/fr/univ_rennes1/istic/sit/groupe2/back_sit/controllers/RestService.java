935005a47076aa851aac8ed9c0c5732c147bcbe7
Abdallah Lachaal <abdallah.lachaal@etudiant.univ-rennes1.fr>
2019-02-12 17:21:14 +0100

    [EVO][BACK] liaison des DAO au service REST

--- back_sit/src/main/java/fr/univ_rennes1/istic/sit/groupe2/back_sit/controllers/RestService.java
+++ back_sit/src/main/java/fr/univ_rennes1/istic/sit/groupe2/back_sit/controllers/RestService.java
@@ -1,21 +1,36 @@
 package fr.univ_rennes1.istic.sit.groupe2.back_sit.controllers;
 
+import fr.univ_rennes1.istic.sit.groupe2.back_sit.dao.MissionDao;
+import fr.univ_rennes1.istic.sit.groupe2.back_sit.dao.PositionDao;
+import fr.univ_rennes1.istic.sit.groupe2.back_sit.models.Mission;
+import fr.univ_rennes1.istic.sit.groupe2.back_sit.models.Position;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class RestService {
 
+    @Autowired
+    private MissionDao missionDao;
+
+    @Autowired
+    private PositionDao positionDao;
+
     @RequestMapping("/missions")
     public ResponseEntity<?> getMissions() {
-        return new ResponseEntity<>(null, HttpStatus.OK);
+        List<Mission> missions = missionDao.findAll();
+        return new ResponseEntity<>(missions, HttpStatus.OK);
     }
 
     @RequestMapping("/positions")
     public ResponseEntity<?> getPositions() {
-        return new ResponseEntity<>(null, HttpStatus.OK);
+        List<Position> positions = positionDao.findAll();
+        return new ResponseEntity<>(positions, HttpStatus.OK);
     }
 }
