1de556d0c1549173c23b96a89704cc1a6a4a9e08
Mouhyi-eddine <moutarajji.mouhyi@gmail.com>
2019-02-12 11:14:01 +0100

    le rajout du package Dao et Services

--- back_sit/src/main/java/fr/univ_rennes1/istic/sit/groupe2/back_sit/dao/MissionDao.java
+++ back_sit/src/main/java/fr/univ_rennes1/istic/sit/groupe2/back_sit/dao/MissionDao.java
@@ -1,9 +1,7 @@
 package fr.univ_rennes1.istic.sit.groupe2.back_sit.dao;
 
+import fr.univ_rennes1.istic.sit.groupe2.back_sit.model.Mission;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-
-import fr.univ_rennes1.istic.sit.groupe2.back_sit.models.Mission;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MissionDao extends MongoRepository<Mission,Long> {
+public interface MissionDao extends JpaRepository<Mission,Long> {
 }
--- back_sit/src/main/java/fr/univ_rennes1/istic/sit/groupe2/back_sit/dao/PositionDao.java
+++ back_sit/src/main/java/fr/univ_rennes1/istic/sit/groupe2/back_sit/dao/PositionDao.java
@@ -1,7 +1,7 @@
 package fr.univ_rennes1.istic.sit.groupe2.back_sit.dao;
 
-import fr.univ_rennes1.istic.sit.groupe2.back_sit.models.Position;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import fr.univ_rennes1.istic.sit.groupe2.back_sit.model.Position;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PositionDao extends MongoRepository<Position,Long> {
+public interface PositionDao extends JpaRepository<Position,Long> {
 }
