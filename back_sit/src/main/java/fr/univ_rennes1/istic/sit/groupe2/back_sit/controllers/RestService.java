63598e96bdca0d3bdfcb649f4688d99fab045c0e
Abdallah Lachaal <abdallah.lachaal@etudiant.univ-rennes1.fr>
2019-02-13 09:03:00 +0100

    [EVO][BACK] Ajout appel REST pour démarrage d'une mission

--- back_sit/src/main/java/fr/univ_rennes1/istic/sit/groupe2/back_sit/controllers/RestService.java
+++ back_sit/src/main/java/fr/univ_rennes1/istic/sit/groupe2/back_sit/controllers/RestService.java
@@ -2,7 +2,9 @@ package fr.univ_rennes1.istic.sit.groupe2.back_sit.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +13,11 @@ public class RestService {
 
     @RequestMapping("/missions")
     public ResponseEntity<?> getMissions() {
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/missions/{id}/start", method = RequestMethod.PATCH)
+    public ResponseEntity<?> startMission(@PathVariable(value = "id") String id) {
         return new ResponseEntity<>(null, HttpStatus.OK);
     }
 
