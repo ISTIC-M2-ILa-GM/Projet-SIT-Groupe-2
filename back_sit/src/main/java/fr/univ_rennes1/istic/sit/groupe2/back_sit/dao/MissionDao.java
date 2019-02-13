4637d8184ad48ade704841cbf4b8002e0a767a13
Mouhyi-eddine <moutarajji.mouhyi@gmail.com>
2019-02-13 09:20:03 +0100

    Modification Long to string in dao

--- back_sit/src/main/java/fr/univ_rennes1/istic/sit/groupe2/back_sit/dao/MissionDao.java
+++ back_sit/src/main/java/fr/univ_rennes1/istic/sit/groupe2/back_sit/dao/MissionDao.java
@@ -5,5 +5,13 @@ package fr.univ_rennes1.istic.sit.groupe2.back_sit.dao;
 import fr.univ_rennes1.istic.sit.groupe2.back_sit.models.Mission;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface MissionDao extends MongoRepository<Mission,Long> {
+import java.time.Instant;
+
+public interface MissionDao extends MongoRepository<Mission,String> {
+
+    public Mission findByName(String name);
+
+    public Mission findByStartDateAndAndEndDate(Instant startDate, Instant endDate);
+
+
 }
--- back_sit/src/main/java/fr/univ_rennes1/istic/sit/groupe2/back_sit/models/Position.java
+++ back_sit/src/main/java/fr/univ_rennes1/istic/sit/groupe2/back_sit/models/Position.java
@@ -2,10 +2,12 @@ package fr.univ_rennes1.istic.sit.groupe2.back_sit.models;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 
 @Getter@Setter
 public class Position {
 
+    @Id
     private String id;
     private float lat;
     private float lon;
