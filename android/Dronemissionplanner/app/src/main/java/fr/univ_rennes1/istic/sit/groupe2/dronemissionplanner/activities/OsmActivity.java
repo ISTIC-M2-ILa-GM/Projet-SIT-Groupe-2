a1cf7a34e701c0bcbb69341c86082d0341cc95fd
AdrienLeblanc <adrienleblanc53@gmail.com>
2019-02-14 11:20:19 +0100

    ajout de la mapView fonctionnelle avec plan de Rennes #4

--- android/Dronemissionplanner/app/src/main/java/fr/univ_rennes1/istic/sit/groupe2/dronemissionplanner/activities/OsmActivity.java
+++ android/Dronemissionplanner/app/src/main/java/fr/univ_rennes1/istic/sit/groupe2/dronemissionplanner/activities/OsmActivity.java
@@ -1,31 +1,86 @@
 package fr.univ_rennes1.istic.sit.groupe2.dronemissionplanner.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.osmdroid.tileprovider.tilesource.ITileSource;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.tileprovider.tilesource.XYTileSource;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
 
 import fr.univ_rennes1.istic.sit.groupe2.dronemissionplanner.R;
 
 
 public class OsmActivity extends AppCompatActivity {
 
-    private MapView         mMapView;
-    private MapController   mMapController;
+    private MapView mMapView;
+    private MapController mMapController;
+    private final GeoPoint RENNES = new GeoPoint(48.117266, -1.6777926);
+    private Marker drone;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_osm);
-        mMapView = (MapView) findViewById(R.id.mapview);
-        mMapView.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
-        mMapView.setBuiltInZoomControls(true);
+        mMapView = findViewById(R.id.mapview);
         mMapController = (MapController) mMapView.getController();
         mMapController.setZoom(13);
-        GeoPoint gPt = new GeoPoint(51500000, -150000);
-        mMapController.setCenter(gPt);
+        mMapController.setCenter(RENNES);
+//        ITileSource tileSource = new XYTileSource ("tiles", 0,
+//                12, 256, ".png", new String[]{});
+        mMapView.setTileSource(TileSourceFactory.MAPNIK);
+    }
+
+    /**
+     * Définit / met à jour la position du drone.
+     *
+     * @param position du Marker
+     */
+    public void updateDronePosition(GeoPoint position) {
+        if (drone != null) {
+            drone.setPosition(position);
+        } else {
+            drone = addMarker(position);
+            drone.setTitle("Drone");
+            mMapView.getOverlays().add(drone);
+        }
+    }
+
+    /**
+     * Ajoute un Marker à la position spécifiée
+     *
+     * @param position du Marker
+     * @return le marker
+     */
+    public Marker addMarker(GeoPoint position) {
+        Marker marker = new Marker(mMapView);
+        marker.setPosition(position);
+        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        mMapView.getOverlays().add(marker);
+        return marker;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        mMapView.onResume(); //needed for compass, my location overlays, v6.0.0 and up
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().save(this, prefs);
+        mMapView.onPause();  //needed for compass, my location overlays, v6.0.0 and up
     }
 
 }
