524a6c2f4c3a1ddf76814e259183d92d70e71b12
vinyard <antoineleval@gmail.com>
2019-02-14 12:07:18 +0100

    persistence des entites des receivers

--- back_sit/src/main/java/fr/univ_rennes1/istic/sit/groupe2/back_sit/amqp/MissionReceiver.java
+++ back_sit/src/main/java/fr/univ_rennes1/istic/sit/groupe2/back_sit/amqp/MissionReceiver.java
@@ -1,6 +1,15 @@
 package fr.univ_rennes1.istic.sit.groupe2.back_sit.amqp;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fr.univ_rennes1.istic.sit.groupe2.back_sit.dao.MissionDao;
+import fr.univ_rennes1.istic.sit.groupe2.back_sit.dao.PositionDao;
+import fr.univ_rennes1.istic.sit.groupe2.back_sit.models.Mission;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author VinYarD
@@ -10,8 +19,22 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 
 public class MissionReceiver {
 	
+	Logger log = Logger.getLogger(MissionReceiver.class.getName());
+	
+	@Autowired
+	ObjectMapper objectMapper;
+	
+	@Autowired
+	private MissionDao missionDao;
+	
 	@RabbitListener(queues = "#{missionQueue.name}")
 	public void receiveMission(String json) throws InterruptedException {
-		// TODO parse json data to Mission object and save() it
+		try {
+			Mission mission = objectMapper.readValue(json, Mission.class);
+			missionDao.save(mission);
+		} catch (IOException e) {
+			log.log(Level.SEVERE, e.getMessage());
+			e.printStackTrace();
+		}
 	}
 }
